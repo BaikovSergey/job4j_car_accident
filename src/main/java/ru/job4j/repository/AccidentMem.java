19aa42f7b1b66034cab3bca95c961e83c6d38895
Baikov Sergey <rayleonard9@gmail.com>
2020-10-25 12:09:14 +0300

    add methods to AccidentMem + AccidentService

--- src/main/java/ru/job4j/repository/AccidentMem.java
+++ src/main/java/ru/job4j/repository/AccidentMem.java
@@ -2,11 +2,39 @@ package ru.job4j.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.Accident;
-import java.util.Map;
+
+import java.util.*;
 
 @Repository
 public class AccidentMem {
 
     private Map<Integer, Accident> accidents;
 
+    public AccidentMem() {
+        this.accidents = new HashMap<>();
+        this.accidents.put(1, new Accident(1, "First accident", "First accident text",
+                "First accident address"));
+        this.accidents.put(2, new Accident(2, "Second accident", "Second accident text",
+                "Second accident address"));
+    }
+
+    public void addAccident(Accident accident) {
+        this.accidents.put(accident.getId(), accident);
+    }
+
+    public Accident findAccidentById(Integer id) {
+        Accident result = new Accident(0, "", "", "");
+        if (accidents.containsKey(id)) {
+            result =  this.accidents.get(id);
+        }
+            return result;
+    }
+
+    public Collection<Accident> findAllAccidents() {
+        List<Accident> result = new ArrayList<>();
+        for (Map.Entry<Integer, Accident> entry : this.accidents.entrySet()) {
+            result.add(entry.getValue());
+        }
+        return result;
+    }
 }
--- src/main/java/ru/job4j/service/AccidentService.java
+++ src/main/java/ru/job4j/service/AccidentService.java
@@ -1,7 +1,26 @@
 package ru.job4j.service;
 
 import org.springframework.stereotype.Service;
+import ru.job4j.model.Accident;
+import ru.job4j.repository.AccidentMem;
+
+import java.util.Collection;
 
 @Service
 public class AccidentService {
+
+    private final AccidentMem accidentMem = new AccidentMem();
+
+    public void addAccident(Accident accident) {
+        this.accidentMem.addAccident(accident);
+    }
+
+    public Collection<Accident> getAllAccidents() {
+        return this.accidentMem.findAllAccidents();
+    }
+
+    public Accident getAccidentById(Integer id) {
+        return this.accidentMem.findAccidentById(id);
+    }
+
 }
